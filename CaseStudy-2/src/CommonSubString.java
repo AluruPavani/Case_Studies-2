f95bb2f17caea6062d313604bbe7548c5184ca55
AluruPavani <69709115+AluruPavani@users.noreply.github.com>
2021-05-27 21:21:49 +0530

    Update CommonSubString.java

--- CaseStudy-2/src/CommonSubString.java
+++ CaseStudy-2/src/CommonSubString.java
@@ -3,16 +3,16 @@ import java.util.HashSet;
 
 public class CommonSubString {
 
-	public static String twoStrings(String s1, String s2) {
+	public static String twoStrings(String str1, String str2) {
 
 		HashSet<Character> string1_chars = new HashSet();
 		HashSet<Character> string2_chars = new HashSet();
 
-		for (int i = 0; i < s1.length(); i++) {
-			string1_chars.add(s1.charAt(i));
+		for (int i = 0; i < str1.length(); i++) {
+			string1_chars.add(str1.charAt(i));
 		}
-		for (int i = 0; i < s2.length(); i++) {
-			string2_chars.add(s2.charAt(i));
+		for (int i = 0; i < str2.length(); i++) {
+			string2_chars.add(str2.charAt(i));
 		}
 		string1_chars.retainAll(string2_chars);
 		if (!string1_chars.isEmpty()) {
@@ -26,9 +26,9 @@ public class CommonSubString {
 
 	public static void main(String[] args) {
 
-		String s1 = "Hello", s2 = "Hi";
+		String string1 = "Hello", string2 = "Hi";
 
-		twoStrings(s1, s2);
+		twoStrings(str1, str2);
 
 	}
 
